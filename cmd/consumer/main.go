11611aff91c0353b1e2bc8060769009b6624075d
Matheus Figueiredo <figueiredo.matheus06@gmail.com>
2022-11-13 15:13:41 -0300

    feat: create main consumer

---
+++ cmd/consumer/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+
+	"github.com/matheusF23/go-rabbitmq-grafana/internal/order/infra/database"
+	"github.com/matheusF23/go-rabbitmq-grafana/internal/order/usecase"
+	"github.com/matheusF23/go-rabbitmq-grafana/pkg/rabbitmq"
+	_ "github.com/mattn/go-sqlite3"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./orders.db")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	repository := database.NewOrderRepository(db)
+	uc := usecase.CalculateFinalPriceUseCase{OrderRepository: repository}
+	ch, err := rabbitmq.OpenChannel()
+	if err != nil {
+		panic(err)
+	}
+	defer ch.Close()
+	out := make(chan amqp.Delivery)
+	go rabbitmq.Consume(ch, out) // Thread 2
+	for msg := range out {
+		var inputDTO usecase.OrderInputDTO
+		err := json.Unmarshal(msg.Body, &inputDTO)
+		if err != nil {
+			panic(err)
+		}
+		outputDTO, err := uc.Execute(inputDTO)
+		if err != nil {
+			panic(err)
+		}
+		msg.Ack(false)
+		fmt.Println(outputDTO)
+	}
+}
